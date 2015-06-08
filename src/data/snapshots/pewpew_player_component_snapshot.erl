8af78d78bb47dc9516126751ffad7e16baca90af
Farruco Sanjurjo <madtrick@gmail.com>
2015-06-08 21:11:48 +0200

    Include the player id in the player snapshot

--- src/data/snapshots/pewpew_player_component_snapshot.erl
+++ src/data/snapshots/pewpew_player_component_snapshot.erl
@@ -5,13 +5,14 @@
 ]).
 
 new(PlayerComponentData) ->
+  Id       = pewpew_player_component_data:id(PlayerComponentData),
   X        = pewpew_player_component_data:x(PlayerComponentData),
   Y        = pewpew_player_component_data:y(PlayerComponentData),
   Rotation = pewpew_player_component_data:rotation(PlayerComponentData),
   Life     = pewpew_player_component_data:life(PlayerComponentData),
 
   #{
-
+     id => Id,
      coordinates => #{ x => X, y => Y },
      rotation => Rotation,
      life => Life
