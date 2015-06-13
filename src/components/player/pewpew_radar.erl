1b255702f05a7d6fc7d528f3f6d161b62b2ea696
Farruco Sanjurjo <madtrick@gmail.com>
2015-06-13 17:26:23 +0200

    Detect players with center under radar

--- src/components/player/pewpew_radar.erl
+++ src/components/player/pewpew_radar.erl
@@ -1,4 +1,5 @@
 -module(pewpew_radar).
+-include_lib("eunit/include/eunit.hrl").
 
 -export([scan/3]).
 
@@ -6,14 +7,26 @@ scan(ArenaComponent, ScanningPlayer, ScanRadius) ->
   Players = pewpew_arena_component:players(ArenaComponent),
   PlayersToCheck = [Player || Player <- Players, Player =/= ScanningPlayer],
 
-  PlayersUnderRadar = players_under_radar(PlayersToCheck),
+  PlayersUnderRadar = players_under_radar(PlayersToCheck, ScanningPlayer, ScanRadius),
 
   #{
     players => PlayersUnderRadar,
     walls => []
   }.
 
-players_under_radar([]) ->
+players_under_radar([], _, _) ->
   [];
-players_under_radar(Players) ->
-  ok.
+players_under_radar(Players, ScanningPlayer, ScanRadius) ->
+  {x, ScanningPlayerX, y, ScanningPlayerY} = pewpew_player_component:coordinates(ScanningPlayer),
+
+  lists:filter(
+    fun(Player) ->
+        {x, X, y, Y} = pewpew_player_component:coordinates(Player),
+
+        A = erlang:abs(X - ScanningPlayerX),
+        B = erlang:abs(Y - ScanningPlayerY),
+        Distance = math:sqrt(A*A + B*B),
+
+        Distance =< ScanRadius
+    end,
+   Players).
--- test/pewpew_radar_tests.erl
+++ test/pewpew_radar_tests.erl
@@ -24,8 +24,42 @@ detects_no_player_when_there_are_no_players_test_() ->
         #{players := PlayersScan, walls := WallScan} = Scan,
 
         [
-         ?_assertEqual(PlayersScan, []),
-         ?_assertEqual(WallScan, [])
+         ?_assertEqual([], PlayersScan),
+         ?_assertEqual([], WallScan)
+        ]
+    end
+  }.
+
+detects_player_with_center_under_radar_test_() ->
+  {setup,
+    fun() ->
+        application:set_env(pewpew, execution_mode, test),
+        pewpew_config:load(),
+
+        PlayersOptions = [
+          [{id, player_id_1}, {x, 200}, {y, 200}],
+          [{id, player_id_2}, {x, 220}, {y, 200}]
+        ],
+        Options = #{
+          arena_options => [{width, 400}, {height, 400}, {pewpew_game_context_data, undefined}],
+          players_options => PlayersOptions
+         },
+        ArenaComponent = pewpew_arena_component_factory:create(Options),
+        ArenaComponent
+    end,
+    fun(ArenaComponent) ->
+        pewpew_arena_component:stop(ArenaComponent)
+    end,
+    fun(ArenaComponent) ->
+        ScanningPlayer = pewpew_arena_component:get_player(ArenaComponent, player_id_1),
+        DetectedPlayer = pewpew_arena_component:get_player(ArenaComponent, player_id_2),
+        Scan   = pewpew_radar:scan(ArenaComponent, ScanningPlayer, 40),
+
+        #{players := PlayersScan, walls := WallScan} = Scan,
+
+        [
+         ?_assertEqual([DetectedPlayer], PlayersScan),
+         ?_assertEqual([], WallScan)
         ]
     end
   }.
