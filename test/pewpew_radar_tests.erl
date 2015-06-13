b416af020e436c562db80a754bc83492f45b0b60
Farruco Sanjurjo <madtrick@gmail.com>
2015-06-13 17:43:41 +0200

    Test that players with center out of radar are not detected

--- test/pewpew_radar_tests.erl
+++ test/pewpew_radar_tests.erl
@@ -63,3 +63,36 @@ detects_player_with_center_under_radar_test_() ->
         ]
     end
   }.
+
+does_not_detect_player_with_center_out_of_radar_test_() ->
+  {setup,
+    fun() ->
+        application:set_env(pewpew, execution_mode, test),
+        pewpew_config:load(),
+
+        PlayersOptions = [
+          [{id, player_id_1}, {x, 100}, {y, 200}],
+          [{id, player_id_2}, {x, 220}, {y, 200}]
+        ],
+        Options = #{
+          arena_options => [{width, 400}, {height, 400}, {pewpew_game_context_data, undefined}],
+          players_options => PlayersOptions
+         },
+        ArenaComponent = pewpew_arena_component_factory:create(Options),
+        ArenaComponent
+    end,
+    fun(ArenaComponent) ->
+        pewpew_arena_component:stop(ArenaComponent)
+    end,
+    fun(ArenaComponent) ->
+        ScanningPlayer = pewpew_arena_component:get_player(ArenaComponent, player_id_1),
+        Scan   = pewpew_radar:scan(ArenaComponent, ScanningPlayer, 40),
+
+        #{players := PlayersScan, walls := WallScan} = Scan,
+
+        [
+         ?_assertEqual([], PlayersScan),
+         ?_assertEqual([], WallScan)
+        ]
+    end
+  }.
