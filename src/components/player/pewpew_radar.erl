d4fe0a67b20767aed60b8d91d6e717c82e2b48bd
Farruco Sanjurjo <madtrick@gmail.com>
2015-06-13 20:16:06 +0200

    Test intersection with walls

--- src/components/player/pewpew_radar.erl
+++ src/components/player/pewpew_radar.erl
@@ -3,16 +3,26 @@
 
 -export([scan/3]).
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% API
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 scan(ArenaComponent, ScanningPlayer, ScanRadius) ->
-  Players = pewpew_arena_component:players(ArenaComponent),
+  {width, ArenaWidth, height, ArenaHeight} = pewpew_arena_component:dimensions(ArenaComponent),
+  Players     = pewpew_arena_component:players(ArenaComponent),
   PlayersToCheck = [Player || Player <- Players, Player =/= ScanningPlayer],
 
   PlayersUnderRadar = players_under_radar(PlayersToCheck, ScanningPlayer, ScanRadius),
+  Walls = intersections_with_walls(ArenaHeight, ArenaWidth, ScanningPlayer, ScanRadius),
 
   #{
     players => PlayersUnderRadar,
-    walls => []
+    walls => Walls
   }.
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Internal
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 players_under_radar([], _, _) ->
   [];
@@ -30,3 +40,89 @@ players_under_radar(Players, ScanningPlayer, ScanRadius) ->
         Distance =< ScanRadius
     end,
    Players).
+
+intersections_with_walls(ArenaHeight, ArenaWidth, ScanningPlayer, ScanRadius) ->
+  lists:flatten([
+                 intersect_left_wall(ArenaHeight, ArenaWidth, ScanningPlayer, ScanRadius),
+                 intersect_right_wall(ArenaHeight, ArenaWidth, ScanningPlayer, ScanRadius),
+                 intersect_bottom_wall(ArenaHeight, ArenaWidth, ScanningPlayer, ScanRadius),
+                 intersect_top_wall(ArenaHeight, ArenaWidth, ScanningPlayer, ScanRadius)
+                ]
+   ).
+
+intersect_left_wall(_, ArenaWidth, ScanningPlayer, ScanRadius) ->
+  LeftWallLine = ArenaWidth,
+  intersect_with_line(vertical, LeftWallLine, ScanningPlayer, ScanRadius).
+intersect_right_wall(_, _, ScanningPlayer, ScanRadius) ->
+  RightWallLine = 0,
+  intersect_with_line(vertical, RightWallLine, ScanningPlayer, ScanRadius).
+intersect_bottom_wall(_, _, ScanningPlayer, ScanRadius) ->
+  BottomWallLine = 0,
+  intersect_with_line(horizontal, BottomWallLine, ScanningPlayer, ScanRadius).
+intersect_top_wall(ArenaHeight, _, ScanningPlayer, ScanRadius) ->
+  TopWallLine = ArenaHeight,
+  intersect_with_line(horizontal, TopWallLine, ScanningPlayer, ScanRadius).
+
+intersect_with_line(WallType, WallLine, ScanningPlayer, ScanRadius) ->
+  {x, PlayerX, y, PlayerY} = pewpew_player_component:coordinates(ScanningPlayer),
+  calculate_coordinates(WallType, WallLine, PlayerX, PlayerY, ScanRadius).
+
+% (C1, C2) = center of the player
+% r = radius of the scan
+%
+% z^2 + y^2 -2*C1*z - 2*C2*y + C1^2 + C2^2 = r^2
+%
+%
+% a*y^2 + b*y + c, where
+%
+% a = 1
+% b = -2*C2
+% c = z^2 - 2*C1*z + C1^2 + C2^2 - R^2
+%
+% y = (-b +- sqrt(b^2 - 4*a*c))/2*a
+%
+% Tangent if b^2 == 4*a*c
+% Intersection if b^2 > 4*a*c
+% No intersection in the other case
+%
+calculate_coordinates(LineType, Z, TC1, TC2, R) ->
+  {C1, C2} = case LineType of
+               horizontal -> {TC2, TC1};
+               vertical -> {TC1, TC2}
+             end,
+
+  A = 1,
+  B = -2 * C1,
+  C = (Z * Z) - 2 * C2 * Z + (C1 * C1) + (C2 * C2) - (R * R),
+
+  FourAC  = 4 * A * C,
+  SquareB = math:pow(B, 2),
+  Temp    = SquareB - FourAC,
+
+  case Temp > 0 of
+    true -> % intersection
+
+      P_1 = erlang:trunc((-B + math:sqrt(Temp)) / 2 * A),
+      P_2 = erlang:trunc((-B - math:sqrt(Temp)) / 2 * A),
+
+      case LineType of
+        horizontal ->
+          [{Z, P_1}, {Z, P_2}];
+        vertical ->
+          [{P_1, Z}, {P_2, Z}]
+      end;
+    false ->
+      case Temp =:= 0.0 of
+        true -> % tangential
+          P_1 = erlang:trunc(-B / 2 * A),
+
+          case LineType of
+            horizontal ->
+              [{Z, P_1}];
+            vertical ->
+              [{P_1, Z}]
+          end;
+        _ -> % no intersection
+          []
+      end
+  end.
--- test/pewpew_radar_tests.erl
+++ test/pewpew_radar_tests.erl
@@ -7,7 +7,7 @@ detects_no_player_when_there_are_no_players_test_() ->
         application:set_env(pewpew, execution_mode, test),
         pewpew_config:load(),
 
-        PlayersOptions = [[{id, player_id_1}]],
+        PlayersOptions = [[{id, player_id_1}, {x, 200}, {y, 200}]],
         Options = #{
           arena_options => [{width, 400}, {height, 400}, {pewpew_game_context_data, undefined}],
           players_options => PlayersOptions
@@ -93,6 +93,85 @@ does_not_detect_player_with_center_out_of_radar_test_() ->
         [
          ?_assertEqual([], PlayersScan),
          ?_assertEqual([], WallScan)
+        ]
+    end
+  }.
+
+detect_right_wall_test_() ->
+  generate_wall_test(
+    [{x, 380}, {y, 200}],
+    [{400, 234}, {400, 165}]
+   ).
+
+detect_right_tangent_wall_test_() ->
+  generate_wall_test(
+    [{x, 360}, {y, 200}],
+    [{400, 200}]
+   ).
+
+detect_top_wall_test_() ->
+  generate_wall_test(
+    [{x, 200}, {y, 380}],
+    [{234, 400}, {165, 400}]
+   ).
+
+detect_tangent_top_wall_test_() ->
+  generate_wall_test(
+    [{x, 200}, {y, 360}],
+    [{200, 400}]
+   ).
+
+detect_left_wall_test_() ->
+  generate_wall_test(
+    [{x, 20}, {y, 200}],
+    [{0, 234}, {0, 165}]
+   ).
+
+detect_left_tangent_wall_test_() ->
+  generate_wall_test(
+    [{x, 40}, {y, 200}],
+    [{0, 200}]
+   ).
+
+
+detect_bottom_wall_test_() ->
+  generate_wall_test(
+    [{x, 200}, {y, 20}],
+    [{234, 0}, {165, 0}]
+   ).
+
+detect_tangent_bottom_wall_test_() ->
+  generate_wall_test(
+    [{x, 200}, {y, 40}],
+    [{200, 0}]
+   ).
+
+generate_wall_test(PlayerCoordinates, Expectations) ->
+  {setup,
+    fun() ->
+        application:set_env(pewpew, execution_mode, test),
+        pewpew_config:load(),
+
+        PlayerOptions = lists:flatten([ PlayerCoordinates | [{id, player_id_1} | []] ]),
+        Options = #{
+          arena_options => [{width, 400}, {height, 400}, {pewpew_game_context_data, undefined}],
+          players_options => [PlayerOptions]
+         },
+        ArenaComponent = pewpew_arena_component_factory:create(Options),
+        ArenaComponent
+    end,
+    fun(ArenaComponent) ->
+        pewpew_arena_component:stop(ArenaComponent)
+    end,
+    fun(ArenaComponent) ->
+        ScanningPlayer = pewpew_arena_component:get_player(ArenaComponent, player_id_1),
+        Scan   = pewpew_radar:scan(ArenaComponent, ScanningPlayer, 40),
+
+        #{players := PlayersScan, walls := WallScan} = Scan,
+
+        [
+         ?_assertEqual([], PlayersScan),
+         ?_assertEqual(Expectations, WallScan)
         ]
     end
   }.
