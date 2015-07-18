e5989fb4ae92b66c8f757f1cf94af58e7ef24858
Farruco Sanjurjo <madtrick@gmail.com>
2015-07-18 11:52:56 +0200

    WIP:move radar out of the client

--- src/components/pewpew_arena_component.erl
+++ src/components/pewpew_arena_component.erl
@@ -23,7 +23,6 @@
 ]).
 
 -define(PLAYER_DOWN(Pid), {'DOWN', _, process, Pid, _}).
--define(COLORS, [<<"red">>, <<"blue">>, <<"green">>]).
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% API
@@ -85,21 +84,12 @@ handle_call(snapshot, _, ArenaComponentData) ->
   Snapshot = pewpew_arena_component_snapshot:new(ArenaComponentData),
   {reply, Snapshot, ArenaComponentData};
 handle_call({create_player, Data}, _, ArenaComponentData) ->
-  Color        = pick_player_color(ArenaComponentData),
-  Id           = pick_player_id(ArenaComponentData),
-  {x, X, y, Y} = pick_player_coordinates(ArenaComponentData),
-  Name         = pick_player_name(ArenaComponentData),
-  Radius       = pick_player_radius(),
+  {ok, Player} = pewpew_arena_component_mod:create_player(ArenaComponentData, Data),
 
-  {ok, Player}                = pewpew_player_component_sup:add_player(
-    pewpew_arena_component_data:pewpew_player_component_sup(ArenaComponentData),
-    pewpew_arena_component_data:pewpew_game_context_data(ArenaComponentData),
-    [{color, Color}, {id, Id}, {x, X}, {y, Y}, {name, Name}, {radius, Radius} | Data]
-  ),
-
-  monitor_player_componet(Player),
   Players               = pewpew_arena_component_data:players(ArenaComponentData),
-  NewArenaComponentData = pewpew_arena_component_data:update(ArenaComponentData, [{players, [Player | Players]}]),
+  UpdatedPlayersList    = [{players, [Player | Players]}],
+  NewArenaComponentData = pewpew_arena_component_data:update(ArenaComponentData, UpdatedPlayersList),
+
   {reply, Player, NewArenaComponentData};
 
 handle_call({get_player, PlayerId}, _, ArenaComponentData) ->
@@ -136,45 +126,5 @@ terminate(_, _) ->
 real_positions_left(ArenaComponentData) ->
   pewpew_arena_component_data:max_number_of_players(ArenaComponentData) - number_of_players(ArenaComponentData).
 
-monitor_player_componet(Player) ->
-  erlang:monitor(process, Player).
-
 number_of_players(ArenaComponentData) ->
   erlang:length(pewpew_arena_component_data:players(ArenaComponentData)).
-
-pick_player_color(ArenaComponentData) ->
-  Players       = pewpew_arena_component_data:players(ArenaComponentData),
-  PlayersColors = [ pewpew_player_component:color(Player) || Player <- Players],
-  [Color | _]   = available_colors(?COLORS, PlayersColors),
-  Color.
-
-available_colors(AllColors, UsedColors) ->
-  lists:filter(fun(X) -> not lists:member(X, UsedColors) end, AllColors).
-
-pick_player_id(_) ->
-  fserlangutils_time:microseconds_since_epoch().
-
-pick_player_coordinates(ArenaComponentData) ->
-  %NOTE: for now I'm harcoding here the radius of the player
-  % Substract the double of the radious and the add the radious
-  % after pick_player_?_coordinate. This is because pick_player_?_coordinate
-  % could return a value < radious.
-  %
-  % radius : 16
-
-  X = pick_player_x_coordinate(pewpew_arena_component_data:width(ArenaComponentData) - 32) + 16,
-  Y = pick_player_y_coordinate(pewpew_arena_component_data:height(ArenaComponentData) - 32) + 16,
-
-  {x, X, y, Y}.
-
-pick_player_x_coordinate(MaxX) ->
-  random:uniform(MaxX).
-
-pick_player_y_coordinate(MaxY) ->
-  random:uniform(MaxY).
-
-pick_player_name(_ArenaComponentData) ->
-  <<"Player">>.
-
-pick_player_radius() ->
-  5. %in px
--- src/components/pewpew_arena_component_mod.erl
+++ src/components/pewpew_arena_component_mod.erl
@@ -2,10 +2,13 @@
 
 -export([
   get_player/2,
+  create_player/2,
   move_player/3,
   dimensions/1,
   update/1
 ]).
+
+-define(COLORS, [<<"red">>, <<"blue">>, <<"green">>]).
 
 get_player(Condition, ArenaComponentData) ->
   MatchingPlayers = find_players_matching_condition(Condition, ArenaComponentData),
@@ -14,6 +17,27 @@ get_player(Condition, ArenaComponentData) ->
     []       -> {ok, undefined};
     [Player] -> {ok, Player}
   end.
+
+create_player(ArenaComponentData, PlayerData) ->
+  Color        = pick_player_color(ArenaComponentData),
+  Id           = pick_player_id(ArenaComponentData),
+  {x, X, y, Y} = pick_player_coordinates(ArenaComponentData),
+  Name         = pick_player_name(ArenaComponentData),
+  Radius       = pick_player_radius(),
+
+  PlayersSupervisor = pewpew_arena_component_data:pewpew_player_component_sup(ArenaComponentData),
+  GameContextData   = pewpew_arena_component_data:pewpew_game_context_data(ArenaComponentData),
+  PlayerConfig      = [{color, Color}, {id, Id}, {x, X}, {y, Y}, {name, Name}, {radius, Radius} | PlayerData],
+
+  {ok, Player}      = pewpew_player_component_sup:add_player(
+    PlayersSupervisor,
+    GameContextData,
+    PlayerConfig
+  ),
+
+  monitor_player_componet(Player),
+
+  {ok, Player}.
 
 move_player(Player, Movement, _) ->
   pewpew_player_component:move(Player, Movement),
@@ -27,8 +51,9 @@ dimensions(ArenaComponentData) ->
 
 update(ArenaComponentData) ->
   Players = pewpew_arena_component_data:players(ArenaComponentData),
+
   PlayersUpdates = lists:map(fun(Player) ->
-    {player, Player, update, pewpew_player_component:update(Player) }
+    {player, Player, update, pewpew_player_component:update(Player)}
   end, Players),
 
   {ok, PlayersUpdates}.
@@ -44,3 +69,46 @@ find_players_matching_condition(Condition, ArenaComponentData) ->
    (pewpew_player_component:id(Player) =:= Condition) or
    (pewpew_player_component:channel(Player) =:= Condition)
   ].
+
+pick_player_id(_) ->
+  fserlangutils_time:microseconds_since_epoch().
+
+pick_player_coordinates(ArenaComponentData) ->
+  %NOTE: for now I'm harcoding here the radius of the player
+  % Substract the double of the radious and the add the radious
+  % after pick_player_?_coordinate. This is because pick_player_?_coordinate
+  % could return a value < radious.
+  %
+  % radius : 16
+
+  ArenaWidth  = pewpew_arena_component_data:width(ArenaComponentData),
+  ArenaHeight = pewpew_arena_component_data:height(ArenaComponentData),
+
+  X = pick_player_x_coordinate(ArenaWidth - 32) + 16,
+  Y = pick_player_y_coordinate(ArenaHeight - 32) + 16,
+
+  {x, X, y, Y}.
+
+pick_player_x_coordinate(MaxX) ->
+  random:uniform(MaxX).
+
+pick_player_y_coordinate(MaxY) ->
+  random:uniform(MaxY).
+
+pick_player_name(_ArenaComponentData) ->
+  <<"Player">>.
+
+pick_player_radius() ->
+  5. %in px
+
+pick_player_color(ArenaComponentData) ->
+  Players       = pewpew_arena_component_data:players(ArenaComponentData),
+  PlayersColors = [ pewpew_player_component:color(Player) || Player <- Players],
+  [Color | _]   = available_colors(?COLORS, PlayersColors),
+  Color.
+
+available_colors(AllColors, UsedColors) ->
+  lists:filter(fun(X) -> not lists:member(X, UsedColors) end, AllColors).
+
+monitor_player_componet(Player) ->
+  erlang:monitor(process, Player).
