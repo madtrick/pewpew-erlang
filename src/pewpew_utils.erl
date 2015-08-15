83345e97ec5ea5c86172b56a775ded69fd3cfe43
Farruco Sanjurjo <madtrick@gmail.com>
2015-08-15 14:05:01 +0200

    Utilities to set values in the maps. Supports nesting

--- src/pewpew_utils.erl
+++ src/pewpew_utils.erl
@@ -2,7 +2,8 @@
 
 -export([
   proplist_to_map/1,
-  get_value_in_map/2
+  get_value_in_map/2,
+  set_value_in_map/3
 ]).
 
 proplist_to_map(Proplist) ->
@@ -25,3 +26,20 @@ get_value_in_map(Keys, Map) ->
   [Key | Tail] = Keys,
   NewMap = maps:get(Key, Map),
   get_value_in_map(Tail, NewMap).
+
+set_value_in_map(Key, Value, Map) when is_atom(Key) ->
+  set_value_in_map([Key], Value, Map);
+set_value_in_map([Key], Value, Map) ->
+  maps:put(Key, Value, Map);
+set_value_in_map(Keys, Value, Map) ->
+  [Key | Tail] = Keys,
+  ValueForKey = maps:get(Key, Map, not_found),
+
+  NewValueForKey = case is_map(ValueForKey) of
+    true ->
+      set_value_in_map(Tail, Value, ValueForKey);
+    false ->
+      set_value_in_map(Tail, Value, #{})
+  end,
+
+  maps:put(Key, NewValueForKey, Map).
--- test/pewpew_utils_tests.erl
+++ test/pewpew_utils_tests.erl
@@ -19,3 +19,25 @@ get_nested_value_in_map_test_() ->
   Value = pewpew_utils:get_value_in_map([key, nested_key], Map),
 
   ?_assertEqual(value, Value).
+
+set_value_in_map_test_() ->
+  Map = #{},
+
+  NewMap = pewpew_utils:set_value_in_map(key, value, Map),
+  Value = pewpew_utils:get_value_in_map(key, NewMap),
+
+  ?_assertEqual(value, Value).
+
+set_value_in_map_for_existing_nested_key_test_() ->
+  Map = #{key => #{nested_key => something, other => value}},
+
+  NewMap = pewpew_utils:set_value_in_map([key, nested_key], value, Map),
+
+  ?_assertEqual(#{key => #{nested_key => value, other => value}}, NewMap).
+
+set_value_in_map_for_non_existing_nested_key_test_() ->
+  Map = #{},
+
+  NewMap = pewpew_utils:set_value_in_map([key, nested_key], value, Map),
+
+  ?_assertEqual(#{key => #{nested_key => value}}, NewMap).
