482c04fdb2daa8523929e2432bb3db1f80a74823
Farruco Sanjurjo <madtrick@gmail.com>
2015-08-15 15:01:35 +0200

    Proplist to map utility function does not transform nested proplists

--- src/pewpew_utils.erl
+++ src/pewpew_utils.erl
@@ -9,13 +9,7 @@
 proplist_to_map(Proplist) ->
   lists:foldl(fun(Element, Map) ->
     {Key, Value} = Element,
-
-    V = case is_list(Value) of
-      true -> proplist_to_map(Value);
-      false -> Value
-    end,
-
-    maps:put(Key, V, Map)
+    maps:put(Key, Value, Map)
   end, #{}, Proplist).
 
 get_value_in_map(Key, Map) when is_atom(Key) ->
--- test/pewpew_utils_tests.erl
+++ test/pewpew_utils_tests.erl
@@ -7,12 +7,6 @@ proplist_to_map_test_() ->
 
   ?_assertEqual(Map, pewpew_utils:proplist_to_map(Proplist)).
 
-nested_proplist_to_map_test_() ->
-  NestedProplist = [{key, [{nested_key, value}]}],
-  NestedMap = #{key => #{nested_key => value}},
-
-  ?_assertEqual(NestedMap, pewpew_utils:proplist_to_map(NestedProplist)).
-
 get_nested_value_in_map_test_() ->
   Map = #{key => #{nested_key => value}},
 
