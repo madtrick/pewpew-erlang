79c6a5d43101ccc795a45d552532ee9f77c2f2e8
Farruco Sanjurjo <madtrick@gmail.com>
2015-11-07 16:27:20 +0100

    Remove more unused modules

--- src/pewpew_command_parser.erl
+++ src/pewpew_command_parser.erl
@@ -14,16 +14,12 @@ parse(Payload) ->
 
   build_command(Type, Data).
 
-build_command(<<"ShotHitPlayerCommand">>, Data) ->
-  pewpew_command_context_data:new(pewpew_shot_hit_player_command_context, pewpew_shot_hit_player_command:fromJSON(Data));
 build_command(<<"RegisterPlayerCommand">>, Data) ->
   pewpew_command_context_data:new(pewpew_register_player_context, register_player_command:fromJSON(Data));
 build_command(<<"MovePlayerCommand">>, Data) ->
   pewpew_command_context_data:new(pewpew_move_player_context, pewpew_move_player_command:fromJSON(Data));
 build_command(<<"PlayerShootCommand">>, Data) ->
   pewpew_command_context_data:new(pewpew_player_shoot_context, pewpew_player_shoot_command:fromJSON(Data));
-build_command(<<"DestroyPlayerCommand">>, Data) ->
-  pewpew_command_context_data:new(pewpew_destroy_player_context, pewpew_destroy_player_command:fromJSON(Data));
 build_command(<<"StartGameCommand">>, Data) ->
   pewpew_command_context_data:new(pewpew_start_game_context, pewpew_start_game_command:fromJSON(Data));
 build_command(<<"ConfigurePlayerCommand">>, Data) ->
